2f0b6700e63abe5e13d5db97f0c1f5c10b0d9b5d
Purva Suryawanshi <87109801+purvapd@users.noreply.github.com>
2023-04-26 18:33:30 +0530

    Add files via upload

---
+++ Second Year/Microprocessor Techniques Laboratory/Assignment_7/example_1.asm
@@ -0,0 +1,32 @@
+section .data
+a dw 2  ; a = 2 (word size)
+b dw 3  ; b = 3 (word size)
+c dw 4  ; c = 4 (word size)
+d dw 5  ; d = 5 (word size)
+result dw 0  ; variable to store the result (word size)
+
+section .text
+global _start
+_start:
+  ; Load a, b, c, d into registers
+  mov ax, [a]
+  mov bx, [b]
+  mov cx, [c]
+  mov dx, [d]
+
+  ; Perform (a + b) and store the result in ax
+  add ax, bx
+
+  ; Perform (c + d) and store the result in cx
+  add cx, dx
+
+  ; Multiply (a + b) * (c + d) and store the result in ax
+  imul ax, cx
+
+  ; Store the result in the result variable
+  mov [result], ax
+
+  ; Exit the program
+  mov eax, 1
+  xor ebx, ebx
+  int 0x80
